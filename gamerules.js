b6ccedabd2a24074252a402feddd1c44f0388ee0
Aaron Roberts <aaronroberts@Aarons-MacBook-Pro.local>
2021-11-02 16:16:10 -0500

    amr

---
+++ gamerules.js
@@ -0,0 +1,20 @@
+class Player {
+    // Part 1: create constructor, decide on properties, and initial values
+    constructor(name,playerNumber) {
+      this.name = name;
+      this.playerNumber=playerNumber;
+    }
+    // Part 2: add methods
+    sayHello() {
+      console.log(`Hello, ${this.name}, welcome to a game of rock paper scissors!`);
+    }
+  
+    
+  module.exports = Player;
+  
+
+
+
+
+
+
