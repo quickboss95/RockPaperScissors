28ebcee601ed15c95b95e27d0fb2bfcb88c6cba3
Aaron Roberts <aaronroberts@Aarons-MacBook-Pro.local>
2021-11-03 11:40:29 -0500

    amr

--- gamerules.js
+++ gamerules.js
@@ -1,16 +1,19 @@
 class Player {
     // Part 1: create constructor, decide on properties, and initial values
-    constructor(name,playerNumber) {
+    constructor(name) {
       this.name = name;
-      this.playerNumber=playerNumber;
     }
     // Part 2: add methods
+    prompt(userInput)
+    
     sayHello() {
       console.log(`Hello, ${this.name}, welcome to a game of rock paper scissors!`);
     }
-  
+    definePlayer(player){
+      this.playerNumber=player  
+    }
     
-  module.exports = Player;
+}
   
 
 
